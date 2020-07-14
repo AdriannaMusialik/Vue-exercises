13dd28dc55588dd61b4cfe97778d5586cbbd281c
Adrianna Musialik <musialik.adrianna@gmail.com>
2020-07-14 22:49:15 +0200

    added back button

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -6,8 +6,13 @@
                 <LoremIpsum add="2p" />
             </p>
         </article>
-        <div class="homeButton">
-            <router-link to="/" >Home</router-link>
+        <div class="buttons">
+            <div class="backButton">
+                <button @click="goBack(-1)">Back</button>
+            </div>
+            <div class="homeButton">
+                <button @click='goTo("/")' >Home</button>
+            </div>
         </div>
     </div>
 </template>
@@ -18,21 +23,33 @@ import LoremIpsum from 'vue-lorem-ipsum';
 export default {
     name: 'Article',
     components: {LoremIpsum},  
+    methods: {
+        goBack (site) {
+           this.$router.go(site);
+        },
+        goTo (site) {
+            this.$router.push(site)
+        }
+    }
 }
+
 </script>
 
 <style>
 .article {
     margin: 20px
 }
-.homeButton {
+
+.buttons {
+    display: flex;
+    justify-content: space-between;
+}
+
+.homeButton, .backButton {
   margin: 20px;
-  font-size: 20px;
+  font-size: 25px;
   text-align: right;
 }
 
-.homeButton a {
-  text-decoration: none;
-  color: black;
-}
+
 </style>
